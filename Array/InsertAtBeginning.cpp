dbeb15ca5dabada5636e568cf45159893dc60c40
kapoormayank <kapoormayank645@gmail.com>
2026-03-07 13:56:22 +0530

    Arrays File Are Create Successfully

--- Array/InsertAtBeginning.cpp
+++ Array/InsertAtBeginning.cpp
@@ -11,7 +11,7 @@ void printArray(vector<int>& arr) {
     cout<<endl;
 }
 
-// Function to insert the beginning position of array
+// Function to insert at beginning position of array
 void insertAtBeginning(vector<int>& arr,int m) {
     arr.insert(arr.begin(),m);
 }
@@ -19,19 +19,19 @@ void insertAtBeginning(vector<int>& arr,int m) {
 // Main function
 int main() {
     int n,m;
-    cout<<"Enter size of first array: ";
+    cout<<"Enter size of array: ";
     cin>>n;
     vector<int> arr(n);
     cout<<"Enter elements: ";
     for(int i=0;i<n;i++) {
         cin>>arr[i];
     }
-    cout<<"Print the first array: ";
+    cout<<"Print the array: ";
     printArray(arr);
     cout<<"Enter element for insert at beginning: ";
     cin>>m;
-    insertAtBeginning(arr,n,m);
-    cout<<"Insert element in array at beignning: ";
+    insertAtBeginning(arr,m);
+    cout<<"Insert element at beignning of an array: ";
     printArray(arr);
     return 0;
 }
--- Array/InsertAtEnd.cpp
+++ Array/InsertAtEnd.cpp
@@ -11,7 +11,7 @@ void printArray(vector<int>& arr) {
     cout<<endl;
 }
 
-// Function to insert the end position of array
+// Function to insert at end position of array
 void insertAtEnd(vector<int>& arr,int m) {
     arr.push_back(m);
 }
@@ -19,19 +19,19 @@ void insertAtEnd(vector<int>& arr,int m) {
 // Main function
 int main() {
     int n,m;
-    cout<<"Enter size of first array: ";
+    cout<<"Enter size of array: ";
     cin>>n;
     vector<int> arr(n);
     cout<<"Enter elements: ";
     for(int i=0;i<n;i++) {
         cin>>arr[i];
     }
-    cout<<"Print the first array: ";
+    cout<<"Print the array: ";
     printArray(arr);
     cout<<"Enter element for insert at end: ";
     cin>>m;
-    insertAtEnd(arr,n,m);
-    cout<<"Insert element in array at beignning: ";
+    insertAtEnd(arr,m);
+    cout<<"Insert element at end of an array: ";
     printArray(arr);
     return 0;
 }
