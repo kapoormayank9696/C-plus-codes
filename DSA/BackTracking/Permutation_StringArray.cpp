36432c7971b7bb0e243047e897e5729db0ff662b
kapoormayank <kapoormayank645@gmail.com>
2026-04-01 21:27:31 +0530

    Data Structure And Algorithms

---
+++ DSA/BackTracking/Permutation_StringArray.cpp
@@ -0,0 +1,47 @@
+// Permutation Generation using Backtracking Algorithm Implementation in C++
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+// Function to generate permutations using backtracking
+void permute(vector<string>& nums, vector<vector<string>>& result, vector<string>& temp,vector<bool>& used) {
+    if(temp.size() == nums.size()) {
+        result.push_back(temp);
+        return;
+    }
+    for(int i = 0; i < nums.size(); i++) {
+        if(used[i]) continue; // Skip used elements
+        used[i] = true; // Mark element as used
+        temp.push_back(nums[i]); // Include element
+        permute(nums, result, temp, used); // Recursive call
+        temp.pop_back(); // Backtrack (remove last element)
+        used[i] = false; // Mark element as unused
+    }
+}
+
+// Main function
+int main() {
+    cout<<"Enter number of elements: ";
+    int n;
+    cin >> n;
+    vector<string> nums(n);
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+    vector<vector<string>> result;
+    vector<string> temp;
+    vector<bool> used(nums.size(), false);
+    permute(nums, result, temp, used);
+    cout << "Permutations are:\n";
+    for(auto permutation : result) {
+        cout << "[ ";
+        for(int i = 0; i < permutation.size(); i++) {
+            cout << permutation[i];
+            // Add a comma except for the last element
+            if (i < permutation.size() - 1) cout << ", "; 
+        }
+        cout << "]\n";
+    }
+    return 0;
+}
