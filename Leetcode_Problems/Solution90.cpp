7d7173af567a9945e2326899b5bb6124dca4cf45
kapoormayank <kapoormayank645@gmail.com>
2026-04-07 18:14:55 +0530

    LeetCode Problems Solution

---
+++ Leetcode_Problems/Solution90.cpp
@@ -0,0 +1,55 @@
+// Leetcode Problem 90: Subsets II
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+    public:
+    // Function to generate subsets using backtracking
+    void subset(vector<int>& nums, vector<vector<int>>& result, vector<int>& temp, int index) {
+        // Add current subset
+        result.push_back(temp);
+        for(int i = index; i < nums.size(); i++) {
+            if(i > index && nums[i] == nums[i-1]) continue; // Skip duplicates
+            // Include element
+            temp.push_back(nums[i]);
+            // Recursive call
+            subset(nums, result, temp, i + 1);
+            // Backtrack (remove last element)
+            temp.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> result;
+        vector<int> temp;
+        sort(nums.begin(), nums.end()); // Sort to handle duplicates
+        subset(nums, result, temp, 0);
+        return result;
+    }
+};
+
+// Main function
+int main() {
+    Solution obj;
+    cout<<"Enter number of elements: ";
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+    vector<vector<int>> result = obj.subsets(nums);
+    cout << "Subsets are:\n";
+    for(auto subset : result) {
+        cout << "[ ";
+        for(int num : subset) {
+            cout << num << " ";
+        }
+        cout << "]\n";
+    }
+    return 0;
+}
+
