0bfe75154a660cc1d3de11336a5094be3f163722
kapoormayank <kapoormayank645@gmail.com>
2026-04-12 20:11:58 +0530

    Data Structure And Algorithms

--- DSA/LinkedList/CircularLinked_List/CircularLL.cpp
+++ DSA/LinkedList/CircularLinked_List/CircularLL.cpp
@@ -13,6 +13,94 @@ class Node {
         data = value;
         next = nullptr;
     }
+    public:
+    Node* head = nullptr;  
+    // Insert Node In Circular Linked List
+    void insert(int data) {
+        Node* newNode=new Node(data);
+        if(head == nullptr) {
+            head->next=newNode;
+            head=newNode;
+        } else {
+            Node* temp=head;
+            while(temp->next!=head) {
+                temp=temp->next;
+            }
+            temp->next=newNode;
+            newNode->next=head;
+        }
+    }
+
+    // Insert Node at beginning of Circular Linked List
+    void insertAtBeginning(int data) {
+        Node* newNode=new Node(data);
+        if(head == nullptr) {
+            head->next=newNode;
+            head=newNode;
+        } else {
+            Node* temp=head;
+            while(temp->next!=head) {
+                temp=temp->next;
+            }
+            temp->next=newNode;
+            newNode->next=head;
+            head=newNode;
+        }
+    }
+
+    // Insert Node at position of Circular Linked List
+    void insertAtPosition(int data, int position) {
+        Node* newNode=new Node(data);
+        if(head == nullptr) {
+            head->next=newNode;
+            head=newNode;
+        } else {
+            if(position == 1) {
+                insertAtBeginning(data);
+                return;
+            }
+            Node* temp=head;
+            int count=1;
+            while(count<position-1 && temp->next!=head) {
+                temp=temp->next;
+                count++;
+            }
+            if(temp->next==head) {
+                insertAtEnd(data);
+                return;
+            }
+            newNode->next=temp->next;
+            temp->next=newNode;
+        }
+    }
+    
+    // Insert Node at end of Circular Linked List
+    void insertAtEnd(int data) {
+        Node* newNode=new Node(data);
+        if(head == nullptr) {
+            head->next=newNode;
+            head=newNode;
+        } else {
+            Node* temp=head;
+            while(temp->next!=head) {
+                temp=temp->next;
+            }
+            temp->next=newNode;
+            newNode->next=head;
+        }
+    }
+
+    // Display Circular Linked List
+    void display() {
+        if(head == nullptr) {
+            cout<<"Circular Linked List Is Empty"<<endl;
+            return;
+        }
+        Node* temp=head;
+        do {
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        } while(temp!=head);
+        cout<<"(head)"<<endl;
+    }
 };
-
-
