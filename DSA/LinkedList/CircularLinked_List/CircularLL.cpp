8032a60811149042de3b1e2000b947a106acebe7
kapoormayank <kapoormayank645@gmail.com>
2026-04-13 22:22:42 +0530

    Data Structure And Algorithms

--- DSA/LinkedList/CircularLinked_List/CircularLL.cpp
+++ DSA/LinkedList/CircularLinked_List/CircularLL.cpp
@@ -13,39 +13,43 @@ class Node {
         data = value;
         next = nullptr;
     }
+};
+
+// Circular Linked List class
+class CircularLinkedList {
     public:
     Node* head = nullptr;  
     // Insert Node In Circular Linked List
     void insert(int data) {
-        Node* newNode=new Node(data);
+        Node* newNode = new Node(data);
         if(head == nullptr) {
-            head->next=newNode;
-            head=newNode;
-        } else {
-            Node* temp=head;
-            while(temp->next!=head) {
-                temp=temp->next;
-            }
-            temp->next=newNode;
-            newNode->next=head;
+            head = newNode;
+            newNode->next = head;
+            return;
         }
+        Node* temp = head;
+        while(temp->next != head) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        newNode->next = head;
     }
 
     // Insert Node at beginning of Circular Linked List
     void insertAtBeginning(int data) {
-        Node* newNode=new Node(data);
+        Node* newNode = new Node(data);
         if(head == nullptr) {
-            head->next=newNode;
-            head=newNode;
-        } else {
-            Node* temp=head;
-            while(temp->next!=head) {
-                temp=temp->next;
-            }
-            temp->next=newNode;
-            newNode->next=head;
-            head=newNode;
+            head = newNode;
+            newNode->next = head;
+            return;
         }
+        Node* temp = head;
+        while(temp->next != head) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        newNode->next = head;
+        head = newNode;
     }
 
     // Insert Node at position of Circular Linked List
@@ -90,6 +94,108 @@ class Node {
         }
     }
 
+    // Update Node at beginning of Circular Linked List
+    void updateAtBeginning(int data) {
+        if(head == nullptr) {
+            cout<<"Circular Linked List Is Empty"<<endl;
+            return;
+        }
+        head->data=data;
+    }
+
+    // Update Node at position of Circular Linked List
+    void updateAtPosition(int data, int position) {
+        if(head == nullptr) {
+            cout<<"Circular Linked List Is Empty"<<endl;
+            return;
+        }
+        Node* temp=head;
+        int count=1;
+        while(count<position && temp->next!=head) {
+            temp=temp->next;
+            count++;
+        }
+        if(temp->next==head) {
+            cout<<"Position Not Found In Circular Linked List"<<endl;
+            return;
+        }
+        temp->data=data;
+    }
+
+    // Update Node at end of Circular Linked List
+    void updateAtEnd(int data) {
+        if(head == nullptr) {
+            cout<<"Circular Linked List Is Empty"<<endl;
+            return;
+        }
+        Node* temp=head;
+        while(temp->next!=head) {
+            temp=temp->next;
+        }
+        temp->data=data;
+    }
+
+    // Delete Node at beginning of Circular Linked List
+    void deleteAtBeginning() {
+        if(head == nullptr) {
+            cout<<"Circular Linked List Is Empty"<<endl;
+            return;
+        }
+        Node* temp=head;
+        while(temp->next!=head) {
+            temp=temp->next;
+        }
+        Node* toDelete=head;
+        head=head->next;
+        temp->next=head;
+        delete toDelete;
+    }
+
+    // Delete Node at position of Circular Linked List
+    void deleteAtPosition(int position) {
+        if(head == nullptr) {
+            cout<<"Circular Linked List Is Empty"<<endl;
+            return;
+        }
+        if(position == 1) {
+            deleteAtBeginning();
+            return;
+        }
+        Node* temp=head;
+        int count=1;
+        while(count<position-1 && temp->next!=head) {
+            temp=temp->next;
+            count++;
+        }
+        if(temp->next==head) {
+            cout<<"Position Not Found In Circular Linked List"<<endl;
+            return;
+        }
+        Node* toDelete=temp->next;
+        temp->next=toDelete->next;
+        delete toDelete;
+    }
+
+    // Delete Node at end of Circular Linked List
+    void deleteAtEnd() {
+        if(head == nullptr) {
+            cout<<"Circular Linked List Is Empty"<<endl;
+            return;
+        }
+        Node* temp=head;
+        Node* prev=nullptr;
+        while(temp->next!=head) {
+            prev=temp;
+            temp=temp->next;
+        }
+        if(prev==nullptr) {
+            head=nullptr;
+        } else {
+            prev->next=head;
+        }
+        delete temp;
+    }
+
     // Display Circular Linked List
     void display() {
         if(head == nullptr) {
@@ -104,3 +210,74 @@ class Node {
         cout<<"(head)"<<endl;
     }
 };
+
+// Main Function
+int main() {
+    cout<<"Enter The Number Of Nodes In Circular Linked List: ";
+    int n;
+    cin>>n;
+    CircularLinkedList circularLL;
+    cout<<"Enter The Data For Nodes In Circular Linked List: ";
+    for(int i=0; i<n; i++) {
+        int data;
+        cin>>data;
+        circularLL.insert(data);
+    }
+    cout<<"Displaying Circular Linked List: ";
+    circularLL.display();
+
+    // Insert Operations In Circular Linked List
+    cout<<"Enter The Data To Insert At Beginning Of Circular Linked List: ";
+    int data;
+    cin>>data;
+    circularLL.insertAtBeginning(data);
+    cout<<"Displaying Circular Linked List: ";
+    circularLL.display();
+    cout<<"Enter The Data To Insert At Position Of Circular Linked List: ";
+    cin>>data;
+    cout<<"Enter The Position To Insert In Circular Linked List: ";
+    int position;
+    cin>>position;
+    circularLL.insertAtPosition(data, position);
+    cout<<"Displaying Circular Linked List: ";
+    circularLL.display();
+    cout<<"Enter The Data To Insert At End Of Circular Linked List: ";
+    cin>>data;
+    circularLL.insertAtEnd(data);
+    cout<<"Displaying Circular Linked List: ";
+    circularLL.display();
+
+    // Update Operations In Circular Linked List
+    cout<<"Enter The Data To Update At Beginning Of Circular Linked List: ";
+    cin>>data;
+    circularLL.updateAtBeginning(data);
+    cout<<"Displaying Circular Linked List: ";
+    circularLL.display();
+    cout<<"Enter The Data To Update At Position Of Circular Linked List: ";
+    cin>>data;
+    cout<<"Enter The Position To Update In Circular Linked List: ";
+    cin>>position;
+    circularLL.updateAtPosition(data, position);
+    cout<<"Displaying Circular Linked List: ";
+    circularLL.display();
+    cout<<"Enter The Data To Update At End Of Circular Linked List: ";
+    cin>>data;
+    circularLL.updateAtEnd(data);
+    cout<<"Displaying Circular Linked List: ";
+    circularLL.display();
+
+    // Delete Operations In Circular Linked List
+    circularLL.deleteAtBeginning();
+    cout<<"Displaying Circular Linked List: ";
+    circularLL.display();
+    cout<<"Enter The Position To Delete In Circular Linked List: ";
+    cin>>position;
+    circularLL.deleteAtPosition(position);
+    cout<<"Displaying Circular Linked List: ";
+    circularLL.display();
+    circularLL.deleteAtEnd();
+    cout<<"Displaying Circular Linked List: ";
+    circularLL.display();
+    return 0;
+}
+
