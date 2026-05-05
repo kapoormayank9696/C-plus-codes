e420fe1f875d96426bdc78c72ed7453d76153b29
kapoormayank9696 <kapoormayank645@gmail.com>
2026-05-05 14:40:58 +0530

    Data Structure And Algorithms

--- DSA/Loops/LCM.cpp
+++ DSA/Loops/LCM.cpp
@@ -1,1 +1,26 @@
 // LCM Algorithm Implementation In C++
+#include<iostream>
+using namespace std;
+// Function to find the LCM from two numbers
+int lcm(int a,int b) {
+    int lcm=0;
+    int k=(a>b)?a:b;
+    for(int i=k;;i+=k) {
+        if(i%a==0 && i%b==0) {
+            lcm=i;
+            break;
+        }
+    }
+    return lcm;
+}
+
+// Main function
+int main() {
+    int a,b;
+    cout<<"Input 1st number for LCM: ";
+    cin>>a;
+    cout<<"Input 2nd number for LCM: ";
+    cin>>b;
+    int result=lcm(a,b);
+    cout<<"The LCM of "<<a<<" and "<<b<<" is : "<<result;
+}
