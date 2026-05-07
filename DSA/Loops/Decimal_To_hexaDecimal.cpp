ac9e2c6402cadc65416077033829ff9067463a51
kapoormayank9696 <kapoormayank645@gmail.com>
2026-05-07 20:43:48 +0530

    Data Structure And Algorithms

--- DSA/Loops/Decimal_To_hexaDecimal.cpp
+++ DSA/Loops/Decimal_To_hexaDecimal.cpp
@@ -10,6 +10,7 @@ void decimalToHexa(int n) {
         '8','9','A','B',
         'C','D','E','F'
     };
+    
     if(n == 0) {
         cout << "The equivalent Hexadecimal Number : 0";
         return;
