5b8743684268901d563b163a039b1ab0fe7045cc
kapoormayank9696 <kapoormayank645@gmail.com>
2026-05-07 21:01:59 +0530

    Data Structure And Algorithms

---
+++ DSA/Loops/HexaDecimal_To_Decimal.cpp
@@ -0,0 +1,21 @@
+// HexaDecimal To Decimal Algorithm Implementation In C++
+#include<iostream>
+using namespace std;
+
+// Main function
+int main() {
+    string hex;
+    cout << "Input any Hexadecimal number: ";
+    cin >> hex;
+
+    // This line converts a hexadecimal string into a decimal integer.
+    // stoi means: string to integer
+    // Syntax : stoi(string, position, base)
+
+    // E.g stoi("1A", 0, 16);  
+    int decimal = stoi(hex, 0, 16);
+
+    cout << "The equivalent Decimal Number : " << decimal;
+
+    return 0;
+}
