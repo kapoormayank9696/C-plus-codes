9cc937c46380c8788156934a263d416c630109c3
kapoormayank9696 <kapoormayank645@gmail.com>
2026-05-09 20:58:16 +0530

    Data Structure And Algorithms

---
+++ DSA/Loops/Natural_Numbers.cpp
@@ -0,0 +1,18 @@
+// Natural Numbers Algorithm Implementation In C++
+#include<iostream>
+using namespace std;
+
+// Main function
+int main() {
+
+    int n;
+    cout<<"Input number of terms: ";
+    cin>>n;
+
+    cout<<"Natural Numbers Display : ";
+    for(int i=1;i<=n;i++) {
+        cout<<i<<" ";
+    }
+    return 0;
+}
+
