d0bdbf46dc33eb3fb7bd84aa0ba6499a127df058
kapoormayank9696 <kapoormayank645@gmail.com>
2026-05-21 12:55:33 +0530

    Leetcode Problems Solution

---
+++ Leetcode_Problems/Solution2657.cpp
@@ -0,0 +1,45 @@
+// LeetCode Problem: 2657. Find the Prefix Common Array of Two Arrays
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+// Class to solve the problem
+class Solution {
+    public:
+    // function to find the prefix common array of two arrays
+    vector<int> findThePrefixCommonArray(vector<int>& A,vector<int>& B) {
+        int n=min(A.size(),B.size());
+        vector<int> freq(n+1,0);
+        vector<int> ans(n,0);
+        int count = 0;
+        for(int i=0;i<n;i++) {
+
+            freq[A[i]]++;
+            if(freq[A[i]] == 2) {
+                count++;
+            }
+
+            freq[B[i]]++;
+            if(freq[B[i]] == 2) {
+                count++;
+            }
+            ans[i] = count;
+        }
+        return ans;
+    }
+};
+
+// Main function
+int main() {
+    vector<int> A = {1, 3, 2, 4};
+    vector<int> B = {3, 1, 2, 4};
+    Solution sol;
+    vector<int> result = sol.findThePrefixCommonArray(A, B);
+    cout<<"Prefix Common Array: ";
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i] << " ";
+    }
+    return 0;
+}
+
