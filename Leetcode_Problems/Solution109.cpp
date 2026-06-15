7432d79cb7927d334ed1c637e23a2f731e0842be
kapoormayank9696 <kapoormayank645@gmail.com>
2026-06-15 21:54:06 +0530

    LeetCode Problems Solution

---
+++ Leetcode_Problems/Solution109.cpp
@@ -0,0 +1,104 @@
+// LeetCode Problem 109: Convert Sorted List to Binary Search Tree
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// Definition for singly-linked list and binary tree node.
+class ListNode {
+    public:
+    int val;
+    ListNode* next;
+
+    // Parameterized Constructor
+    ListNode(int x) {
+        this->val = x;
+        this->next = nullptr;
+    }
+};
+
+// Definition for a binary search tree node.
+class TreeNode {
+    public:
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    // Parameterized Constructor
+    TreeNode(int x) {
+        this->val = x;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+};
+
+// Function to find the middle element of the linked list
+ListNode* findMiddle(ListNode* head) {
+    ListNode* slow = head;
+    ListNode* fast = head;
+    ListNode* prev = nullptr;
+
+    // Fast and slow pointer Method to find the middle node of the linked list
+    while(fast != nullptr && fast->next != nullptr) {
+        prev = slow;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    if(prev != nullptr) {
+        prev->next = nullptr; // Split the linked list into two halves
+    }
+    return slow; // Return the middle node
+}
+
+// Function to convert sorted linked list to binary search tree
+TreeNode* sortedListToBST(ListNode* head) {
+    if(head == nullptr) {
+        return nullptr;
+    }
+    if(head->next == nullptr) {
+        return new TreeNode(head->val);
+    }
+    ListNode* mid = findMiddle(head);
+    
+    // Create a new tree node
+    TreeNode* root = new TreeNode(mid->val); 
+
+    // Recursively build the left subtree
+    root->left = sortedListToBST(head);
+    // Recursively build the right
+    root->right = sortedListToBST(mid->next);
+
+    return root;
+}
+
+// Function to print the binary search tree(for testing purposes)
+void Traversal(TreeNode* root) {
+    if(root == nullptr) return;
+
+    Traversal(root->left);
+    cout << root->val << " ";
+    Traversal(root->right);
+}
+
+// Main function
+int main() {
+    vector<int> sortedList = {-10, -3, 0, 5, 9}; // Example sorted linked list
+    ListNode* head = nullptr;
+    ListNode* current = nullptr;
+    // Create the linked list from the sorted array
+    for(int num : sortedList) {
+        if(head == nullptr) {
+            head = new ListNode(num);
+            current = head;
+        } else {
+            current->next = new ListNode(num);
+            current = current->next;
+        }
+    }
+    TreeNode* bstRoot = sortedListToBST(head); // Convert the sorted linked list to a binary search tree
+    cout << "Traversal of the BST: ";
+    Traversal(bstRoot); // Print the in-order traversal of the BST
+    return 0;
+}
+
+
---
+++ Leetcode_Problems/Solution112.cpp
@@ -0,0 +1,77 @@
+// Leetcode Problem 112: Path Sum
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Class of Binary Tree Node
+class TreeNode {
+    public:
+    
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+    
+    // Parameterized Constructor
+    TreeNode(int data) {
+        this->data = data;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+};
+
+// Class to Build the Binary Tree
+class BinaryTree {
+    public:
+    int index = -1;
+    
+    TreeNode* BuildTree(TreeNode* &root, vector<int>& nodes) {
+        index++;
+
+        if (index >= nodes.size() || nodes[index] == -1) {
+            return nullptr;
+        }
+
+        TreeNode* newRoot = new TreeNode(nodes[index]);
+
+        newRoot->left = BuildTree(newRoot->left, nodes);
+        newRoot->right = BuildTree(newRoot->right, nodes);
+
+        return newRoot;
+    }
+
+    // Function to check the path sum in the binary tree
+    bool checkPathSum(TreeNode* root, int targetSum) {
+        if(root == nullptr) {
+            return false;
+        }
+        if(root->left == nullptr && root->right == nullptr) {
+            return targetSum == root->data;
+        }
+        return checkPathSum(root->left, targetSum - root->data) || checkPathSum(root->right, targetSum - root->data);
+    }
+
+    // Function to display the Binary Tree
+    void display(TreeNode* root) {
+        if(root == nullptr) {
+            return;
+        }
+        cout << root->data << " ";
+        display(root->left);
+        display(root->right);
+    }
+};
+
+
+// Main function
+int main() {
+    BinaryTree bt;
+    TreeNode* root = nullptr;
+    vector<int> nodes = {5, 4, 11, 7, -1, -1, 2, -1, -1, -1, 8, 13, -1, -1, 4, -1, 1, -1, -1};
+    root = bt.BuildTree(root, nodes);
+    cout << "Binary Tree: ";
+    bt.display(root);
+    int targetSum = 22;
+    cout << "\nPath with sum is " << targetSum << " exists in the binary tree: " << boolalpha << bt.checkPathSum(root, targetSum) << endl;
+    return 0;
+}
+
