acaa3103a4be1844e097a4d2b087def65c4a06f8
kapoormayank9696 <kapoormayank645@gmail.com>
2026-09-11 20:14:56 +0530

    LeetCode Problems Solution

---
+++ Leetcode_Problems/Solution485.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// Solution Class
+class Solution {
+    public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count = 0, max = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 1) {
+                max = max(count, max);
+                count = 0;
+                continue;
+            }
+            count++;
+        }
+        max = max(count, max);
+        return max;
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {1,1,0,1,1,1,0,1,1};
+    cout << solution.findMaxConsecutiveOnes(nums) << endl;
+    return 0;
+}
