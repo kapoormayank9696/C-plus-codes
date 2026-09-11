b991e5994af5b5e3650b088aa968c036f1bacbbb
kapoormayank9696 <kapoormayank645@gmail.com>
2026-09-11 19:52:50 +0530

    Data Structure And Algorithms

--- DSA/Graphs/RottingOrgane.cpp
+++ DSA/Graphs/RottingOrgane.cpp
@@ -60,6 +60,14 @@ class Graph {
             }
         }
         
+        for(int i=0; i<rows; i++) {
+            for(int j=0; j<cols; j++) {
+                if(grid[i][j] == 1 && !visited[i][j]) {
+                    return -1;
+                }
+            }
+        }
+        
         return ans;
     }
 };
