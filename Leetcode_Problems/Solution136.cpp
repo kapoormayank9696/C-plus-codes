a5744b4916967ff1c59018827b70b6189505fae8
kapoormayank9696 <kapoormayank645@gmail.com>
2026-09-14 18:48:07 +0530

    Data Structure And Algorithm

---
+++ Leetcode_Problems/Solution136.cpp
@@ -0,0 +1,16 @@
+// LeetCode Problem 136: Single Number
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    vector<int> nums = {4, 1, 2, 1, 2};
+    int result = 0;
+
+    for (int num : nums) {
+        result = result ^ num; // XOR operation
+    }
+
+    cout << "The single number is: " << result << endl;
+    return 0;
+}
