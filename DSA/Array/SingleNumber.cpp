65cbb96b83e2a64d3141a0af70c4b0c0f9102dbd
kapoormayank9696 <kapoormayank645@gmail.com>
2026-09-14 19:07:43 +0530

    Data Structure And Algorithm

---
+++ DSA/Array/SingleNumber.cpp
@@ -0,0 +1,18 @@
+// Single Number Algorithm Implementation In C++
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Main function
+int main() {
+    vector<int> nums = {4, 1, 2, 1, 2};
+    int result = 0;
+
+    for (int num : nums)
+    {
+        result = result ^ num; // XOR operation
+    }
+
+    cout << "The single number is: " << result << endl;
+    return 0;
+}
